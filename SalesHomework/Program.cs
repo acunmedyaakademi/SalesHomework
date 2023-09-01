0ee0dd1243e1b0d07d5e3353b4ee336acd18cb63
grethrain <efeyakar2004@outlook.com>
2023-09-01 23:35:25 +0300

    Add project files.

---
+++ SalesHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SalesHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******** EN UYGUN SATIŞ ********\n");
+            Dictionary<string, string> product = new Dictionary<string, string> { };
+            product = SalesHomework.SqlConnections.GetProduct();
+
+        }
+    }
+}
---
+++ SalesHomework/SqlConnections.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SalesHomework
+{
+    internal class SqlConnections
+    {
+        IDictionary<string, string> GetProduct ()
+        {
+            Dictionary<string,string> productList = new Dictionary<string,string> ();
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand product = new SqlCommand(" select ProductsName , ProductsPrices from [HOMEWORK3_1].[dbo].[Products]",sqlConnection);
+            SqlDataReader read = product.ExecuteReader();
+            for (int i = 0; i < read.FieldCount; i++)
+            {
+                productList.Add(read["ProductsName"].ToString(), read["ProductsPrices"].ToString());
+            }
+            return productList;
+        }
+        void Register ()
+        {
+
+        }
+    }
+}
