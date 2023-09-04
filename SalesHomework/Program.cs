ee0c75e8ca49261154890461ad7670a7d91bfb5a
grethrain <efeyakar2004@outlook.com>
2023-09-04 17:39:35 +0300

    .

--- SalesHomework/Program.cs
+++ SalesHomework/Program.cs
@@ -9,12 +9,259 @@ namespace SalesHomework
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("******** EN UYGUN SATIŞ ********\n");
             
-            var product = SqlConnections.GetProduct();
-            Console.WriteLine(product);
+            ;
+            
+            Console.WriteLine("******** EN UYGUN SATIŞ ********\n");
+             string customerId = access();
+            
+            Console.Write("Ürünleri görmek için lütfen 1'i tuşlayın: ");
+
+            while (true)
+            {
+                int result = Convert.ToInt16(Console.ReadLine());
+                if (result == 1)
+                {
+                    GetProduct(0);
+                    break;
+                }
+                else 
+                { 
+                    Console.WriteLine("Hatalı tuşlama yaptınız. Tekrar deneyiniz"); 
+                }
+            }
+
+            string[] shoppinglist = ShoppingCart();
+            PrintBasket(shoppinglist, customerId);
+            Rate(shoppinglist, customerId);
+            AvgRate(customerId);
+        }
+
+        static void AvgRate(string csId)
+        {
+            Console.WriteLine("Kullanıcının aldığı ürünler ve puanları aşağıdadır.");
+            SqlConnections.GetRatelist(csId);
+        }
+
+
+        static string[] ShoppingCart()
+        {
+            int shoppingleng = 0;
+            int j = 0;
+            Console.WriteLine("Sepete eklemek istediğiniz ürünleri birer boşluk bırakarak satır numarasını giriniz. ");
+            string[] shoppingList = Console.ReadLine().Split(' ');
+            shoppingleng = shoppingList.Length;
+            Console.Write("Başka bir şey eklemek istiyor musunuz(y/n)? : ");
+            string answer = Console.ReadLine().ToLower();
+            if ( answer== "y")
+            {
+                Console.WriteLine("Sepete eklemek istediğiniz ürünleri birer boşluk bırakarak satır numarasını giriniz. ");
+                string[] shoppingList2 = Console.ReadLine().Split(' ');
+                Array.Resize(ref shoppingList, shoppingList2.Length+ shoppingList.Length);
+                for (int i = shoppingleng; i < shoppingList.Length; i++)
+                {
+                    shoppingList[i] = shoppingList2[j];
+                    j += 1;
+                }
+                Console.WriteLine("Sepetinizi onaylıyor musunuz(y/n)?");
+
+            }
+            else if (answer == "n")
+            {
+                Console.WriteLine("Sepetinizi onaylıyor musunuz(y/n)?");
+                answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    
+                }
+                else if (answer =="n")
+                {
+                    Console.WriteLine("Sepete ürün eklemek için 1, ürün çıkartmak için 2 yi tuşlayınız :");
+                    answer = Console.ReadLine().ToLower();
+                    if (answer == "1") 
+                    {
+                        j = 0;
+                        Console.WriteLine("Eklemek istediğiniz ürünlerin satır numarasını birer boşluk bırakarak giriniz.");
+                        string[] newlist = Console.ReadLine().Split(' ');
+                        Array.Resize(ref shoppingList, newlist.Length + shoppingList.Length);
+                        for (int i = shoppingleng; i < shoppingList.Length; i++)
+                        {
+                            shoppingList[i] = newlist[j];
+                            j += 1;
+                        }
+
+                    }
+                    else if (answer =="2")
+                    {
+                        Console.WriteLine("çıkartmak istediğiniz ürünlerin satır numarasını birer boşluk bırakarak giriniz.");
+                        string[] newlist = Console.ReadLine().Split(' ');
+
+                        foreach (var item in newlist)
+                        {
+                            for (int i = 0; i < shoppingList.Length; i++)
+                            {
+                                if (shoppingList[i] == item)
+                                {
+                                    Array.Clear(shoppingList, i, 1);
+                                }
+                                
+                            } 
+                        }
+                    }   
+                }
+                else
+                {
+                    Console.WriteLine("Haatalı tuşlama yaptınız.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Haatalı tuşlama yaptınız.");
+            }
+            return shoppingList;
+        }
+
+        static void Rate(string[] shoppinglist,string csId)
+        {
+            var product = GetProduct(1);
+            List<int> pointlist = new List<int>();
+            foreach (var item in shoppinglist)
+            {
+                
+                Console.WriteLine($"{product.Item2[Convert.ToInt32(item)-1]} için 1-5 arasında puan veriniz.");
+                int rate = Convert.ToInt32(Console.ReadLine());
+                pointlist.Add(rate);
+                                
+            }
+            Tuple<List<int>, string[],string> values = new Tuple<List<int>, string[],string>(pointlist, shoppinglist,csId);
+            Console.WriteLine(SqlConnections.InsertRate(values, csId));
+
+            
+        }
+
+        static void PrintBasket(string[] shoppingList,string csId)
+        {
+            int TotalPrices = 0;
+            
+            var productsInformation = SqlConnections.GetProduct();
+            List<string> ProductCart = new List<string>();
+            List<int> ProductPrices = new List<int>();
+            foreach (var item in shoppingList)
+            {
+                ProductCart.Add(productsInformation.Item2[Convert.ToInt32(item) - 1]);
+                ProductPrices.Add(Convert.ToInt32(productsInformation.Item1[Convert.ToInt32(item) - 1]));
+            }
+            Console.WriteLine("******************  SEPETİNİZ  ******************");
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("Satır Sayısı\tÜrünler\t\t Fiyat");
+            Console.WriteLine("*************\t*******\t\t *****");
+            for (int i = 0; i < ProductCart.Count(); i++)
+            {
+                Console.WriteLine($"  {i + 1}\t\t{ProductCart[i]}\t\t{ProductPrices[i]}");
+                TotalPrices = TotalPrices + ProductPrices[i];
+            }
+            Console.WriteLine($"Toplam Sepet Tutarı : {TotalPrices}");
+        }
+
+        static Tuple<List<int>,List<string>> GetProduct(int select)
+        {
+            var productsInformation = SqlConnections.GetProduct();
+            if (select != 1)
+            {
+                Console.WriteLine("Satır Sayısı\tÜrünler\t\t Fiyat");
+                Console.WriteLine("*************\t*******\t\t *****");
+                for (int i = 0; i < productsInformation.Item1.Count; i++)
+                {
+                    Console.WriteLine($"  {i + 1}\t\t{productsInformation.Item2[i]}\t\t{productsInformation.Item1[i]}");
+                }
+            }
+            return productsInformation;
+            
+        }
+
+        static string access()
+        {
+            Console.WriteLine("Giriş Yapmak için 1 kayıt olmak için 0 yazınız.");
+            string customerId = "";
+            int registerQuestion = Convert.ToInt32(Console.ReadLine());
+            if (registerQuestion == 1)
+            {
+                customerId = Login();
+            }
+            else if (registerQuestion == 0)
+            {
+                customerId = CreateNewAccount();
+            }
+            return customerId;
+        }
+
+
+        static string CreateNewAccount()
+        {
+            string password;
+            string passwordcheck;
+            string name;
+            string username;
+            Console.Write("Adı soyad giriniz :");
+            name = Console.ReadLine();
+            Console.Write("E-posta adresinizi giriniz: ");
+            username = Console.ReadLine();
+            while (true)
+            {
+                Console.Write("Şifre  giriniz :");
+                password = Console.ReadLine();
+                Console.Write("Şifrenizi tekrar giriniz :");
+                passwordcheck = Console.ReadLine();
+                if (password == passwordcheck)
+                {
+                    break;
+                }
+            }
+            
+            DateTime now = DateTime.Now;
+            string customerStatus = "Active";
+            string customerId="";
+            bool accontCheck = SqlConnections.Register(name, username, password, now, customerStatus);
+            if (accontCheck == true)
+            {
+                Console.WriteLine("Hesabınız başarılı bir şekilde oluşturuldu.Bir kaç saniye sonra giriş yapabilirsiniz.");
+                customerId = Login();
+                
+            }
+            return customerId;
+        }
+        static string Login()
+        {
+            string customerId;
+            while (true)
+            {
+
+                Console.Write("Kullanıcı Adı giriniz(e-posta) :");
+                string username = Console.ReadLine();
+                if (username =="-1")
+                {
+                    CreateNewAccount();
+                }
+                Console.Write("Şifre giriniz :");
+                string password = Console.ReadLine();
+                var userCheck = SqlConnections.Login(username, password);
+                var returnList = userCheck.Split(',');
+                if (returnList[0] == "Active")
+                {
+                    
+                    customerId = returnList[1];
+                    Console.WriteLine($" CustomerId: {customerId}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(returnList[0]);
+                }
+            }
+            return customerId;
 
         }
     }
--- SalesHomework/SqlConnections.cs
+++ SalesHomework/SqlConnections.cs
@@ -5,29 +5,128 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections;
 
 namespace SalesHomework
 {
     public class SqlConnections
     {
-        public static IDictionary<string, string> GetProduct ()
-        {
-            Dictionary<string,string> productList = new Dictionary<string,string> ();
-            SqlConnection sqlConnection;
-            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3;Integrated Security=True";
-            sqlConnection = new SqlConnection(connectionString);
-            sqlConnection.Open();
-            SqlCommand product = new SqlCommand(" select ProductsName , ProductsPrices from [HOMEWORK3_1].[dbo].[Products]",sqlConnection);
-            SqlDataReader read = product.ExecuteReader();
-            for (int i = 0; i < read.FieldCount; i++)
-            {
-                productList.Add(read["ProductsName"].ToString(), read["ProductsPrices"].ToString());
-            }
-            return productList;
-        }
-        void Register ()
+        public static Tuple<List<int>, List<string>> GetProduct()
         {
 
+            List<int> productPrices = new List<int>();
+            List<string> products = new List<string>();
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3_1;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand product = new SqlCommand(" select ProductsName , ProductsPrices from Products", sqlConnection);
+            SqlDataReader read = product.ExecuteReader();
+            while (read.Read())
+            {
+
+                productPrices.Add(Convert.ToInt32(read["ProductsPrices"]));
+                products.Add(read["ProductsName"].ToString());
+            }
+            sqlConnection.Close();
+            return Tuple.Create(productPrices, products);
+
         }
+        
+        
+        public static string Login(string user, string pass)
+        {
+            string status = "";
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3_1;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand product = new SqlCommand($" select CustomerStatus, CustomerId from Customer WHERE CustomerAccount='{user}' AND CustomerPassword='{pass}'", sqlConnection);
+            var result = product.ExecuteScalar();
+            if (result != null)
+            {
+                SqlDataReader read = product.ExecuteReader();
+                while (read.Read())
+                {
+                    status = read[0].ToString();
+                    string csid = read["CustomerId"].ToString();
+                    status = status + "," + csid;
+                }
+                    
+            }
+            else
+            {
+                status = "Kullanıcı Adı Veya Şifre Hatalı Lütfen Tekrar Deneyiniz";
+
+            }
+            sqlConnection.Close();
+            return status;
+        }
+
+        public static bool Register(string user, string username, string pass, DateTime date, string status)
+        {
+            string result = "";
+            bool returnValue = false;
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3_1;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand checkAccont = new SqlCommand($"SELECT COUNT(*) FROM Customer WHERE CustomerAccount = '{username}' ", sqlConnection);
+            SqlDataReader read = checkAccont.ExecuteReader();
+            while (read.Read())
+            {
+                result = read[0].ToString();
+            }
+            read.Close();
+            if (result == "1")
+            {
+                returnValue = false;
+            } else if (result == "0")
+            {
+                SqlCommand newuser = new SqlCommand($"INSERT INTO Customer (CustomerNameSurname,CustomerAccount,CustomerPassword,AccountCreatingDate,CustomerStatus) VALUES ('{user}','{username}','{pass}','{date}','{status}')", sqlConnection);
+                read = newuser.ExecuteReader();
+                read.Close();
+                returnValue = true;
+
+            }
+            sqlConnection.Close();
+            return returnValue;
+        }
+
+        public static string InsertRate(Tuple<List<int>, string[], string> values,string csId)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3_1;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            for (int i = 0; i < values.Item1.Count; i++)
+            {
+                SqlCommand insertRate = new SqlCommand($"INSERT INTO Sales (ProductsId,CustomerId,ProductRate) VALUES ('{values.Item2[i]}','{values.Item3}','{values.Item1[i]}')", sqlConnection);
+                SqlDataReader read = insertRate.ExecuteReader();
+                read.Close();
+            }
+            sqlConnection.Close();
+            return "işlem başarılı";
+        }
+
+        public static void GetRatelist(string csId)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=GRETHRAIN\sqlexpress;Initial Catalog=HOMEWORK3_1;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand GetRateList = new SqlCommand($"select S.ProductsId,P.ProductsName,P.ProductsPrices,S.ProductRate FROM Sales S  left join Products P on S.ProductsId = P.ProductsId WHERE CustomerId= {Convert.ToInt32(csId)}",sqlConnection);
+            SqlDataReader read = GetRateList.ExecuteReader();
+            List<int> rate = new List<int>();
+            while (read.Read())
+            {
+                Console.WriteLine($"{read[1]}\t\t{read[2]}\t\t{read[3]}");
+                rate.Add(Convert.ToInt32(read[3]));
+            }
+            Console.WriteLine($"kullanıcının ürünlere verdiği ortalama puan: {rate.Average()}");
+            Console.ReadKey();
+            read.Close();
+        }
+
     }
 }
